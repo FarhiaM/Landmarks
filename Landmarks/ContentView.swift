cb7e5a4e25c4833dc1e1edfb95499a40af5f275d
Farhia <farhia.fcb@gmail.com>
2020-06-28 16:54:56 +0200

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Farhia Mohamed Muse on 2020-06-28.
+//  Copyright © 2020 Farhia Mohamed Muse. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
